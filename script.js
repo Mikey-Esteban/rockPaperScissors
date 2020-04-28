8e80eb2e1a4bb74009f7b7a33e6301a0dc3a07a9
Mikey Esteban <michaelp.esteban@gmail.com>
2020-04-28 10:23:54 -0400

    edited win/lose text

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 console.log("script is running");
 
+let button = document.querySelector(".btn");
+button.addEventListener('click', play);
+
 
 function play(){
-  winnerIs(userChoice(), computerChoice())
+  let message = winnerIs(userChoice(), computerChoice());
+  alert(message);
 }
 
 
 //determine winner
 function winnerIs(user, comp) {
   if (user == 'rock' && comp == 'paper'){
-    return 'You lost =('
+    return 'You lost =( paper beats rock'
   }
   else if (user == 'rock' && comp == 'scissor') {
-    return 'You won!'
+    return 'You won! rock beats scissor'
   }
   else if (user == 'paper' && comp == 'scissor') {
-    return 'You lost!'
+    return 'You lost! scissor beats paper'
   }
   else if (user == 'paper' && comp == 'rock') {
-    return 'You won!'
+    return 'You won! paper beats rock'
   }
   else if (user == 'scissor' && comp == 'rock') {
-    return 'You lost!'
+    return 'You lost! rock crushes scissor'
   }
   else if (user == 'scissor' && comp == 'paper') {
-    return 'You won!'
+    return 'You won! scissor cuts paper'
   }
   else{
-    return 'You both tied'
+    return 'You both picked ' + user
   }
 }
 
@@ -35,11 +39,12 @@ function winnerIs(user, comp) {
 // ask user for choice
 function userChoice() {
   let choice = prompt("what is your pick? (rock paper scissor)")
+  choice = choice.toLowerCase();
   return choice
 }
 
 // computer picks between rock, paper, scissor
-function computerChoice() => {
+function computerChoice() {
   let choice = Math.floor(Math.random() * 3);
   if (choice == 0){
     return 'rock';
